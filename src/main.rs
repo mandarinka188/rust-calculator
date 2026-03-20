58ee8a249a5a86b13aa2f5cf63966b4b6fa3c15c
mandarinka188 <uvixamadara@email.com>
2026-03-20 21:43:55 +0200

    calculator 2.0

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,52 @@ use std::io;
 fn main () {
    println!("Ти в калькуляторі!!!");
    println!("Введи перше число: ");
+   let t1: f64 = loop {
    let mut t1 = String::new();
-   io::stdin().read_line(&mut t1).unwrap();
-   let t1: f64 = t1.trim().parse().unwrap();
+   io::stdin().read_line(&mut t1).expect("Помилка вводу");
+
+   match t1.trim().parse() {
+   Ok(number)=> break number,
+   Err(_)=> println!("Напиши число!!!"),
+}
+};
+   
+   
    
    println!("обери дію яку будеш робити: ");
    println!("1: (+)");
    println!("2: (-)");
    println!("3: (*)");
    println!("4: (/)");
+   let u: u32 = loop {
    let mut u = String::new();
-   io::stdin().read_line(&mut u).unwrap();
-   let clean_u = u.trim();
+   io::stdin().read_line(&mut u);
+
+   match u.trim().parse() {
+   Ok(number)=> break number,
+   Err(_)=> println!("Напиши число!!!"),
+}
+};
+   
    
 
+
    println!("Введи друге число: ");
+   let t2: f64 = loop {
    let mut t2 = String::new();
-   io::stdin().read_line(&mut t2).unwrap();
-   let t2: f64 = t2.trim().parse().unwrap();
+   io::stdin().read_line(&mut t2);
+
+   match t2.trim().parse() {
+   Ok(number)=> break number,
+   Err(_)=> println!("Напиши число!!!"),
+}
+};
   
-   match clean_u {
-       "1" => println!("Ось твоя відповідь {}", t1 + t2),
-       "2" => println!("Ось твоя відповідь {}", t1 - t2),
-       "3" => println!("Ось твоя відповідь {}", t1 * t2),
-       "4" => println!("Ось твоя відповідь {}", t1 / t2),
+   match u {
+       1 => println!("Ось твоя відповідь {}", t1 + t2),
+       2 => println!("Ось твоя відповідь {}", t1 - t2),
+       3 => println!("Ось твоя відповідь {}", t1 * t2),
+       4 => println!("Ось твоя відповідь {}", t1 / t2),
         _ => println!("Ти вів не число:("),
    }
 }
